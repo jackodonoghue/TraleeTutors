d2ea0a7654e85a8f4e4cde99ec45389b488ed8fc
Jack O'Donoghue <jacklodonoghue@gmail.com>
2019-10-10 14:43:48 +0100

    Signed-off-by: Jack O'Donoghue <jacklodonoghue@gmail.com>

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -13,3 +13,39 @@ mongoose.connection.on('disconnected', () => {
 console.log('Mongoose disconnected'); 
 });
     
+const readLine = require ('readline');
+if (process.platform === 'win32'){
+const rl = readLine.createInterface ({
+input: process.stdin,
+output: process.stdout
+});
+rl.on ('SIGINT', () => {
+process.emit ("SIGINT");
+});
+}
+
+const gracefulShutdown = (msg, callback) => { 
+mongoose.connection.close( () => { 
+console.log(`Mongoose disconnected through ${msg}`); 
+callback(); 
+});
+};
+    
+process.once('SIGUSR2', () => { 
+gracefulShutdown('nodemon restart', () => { 
+process.kill(process.pid, 'SIGUSR2'); 
+});
+});
+process.on('SIGINT', () => { 
+gracefulShutdown('app termination', () => { 
+process.exit(0); 
+});
+});
+process.on('SIGTERM', () => { 
+gracefulShutdown('Heroku app shutdown', () => { 
+process.exit(0); 
+});
+});
+
+
+    
