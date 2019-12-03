4b7eafb3dc57a5559b6e743aa85583c3332ea87e
Jack O'Donoghue <jacklodonoghue@gmail.com>
2019-12-03 19:59:45 +0000

    Signed-off-by: Jack O'Donoghue <jacklodonoghue@gmail.com>

--- app_server/controllers/grinds.js
+++ app_server/controllers/grinds.js
@@ -3,7 +3,7 @@ const apiOptions = {
 server: 'http://localhost:3000',
 };
 if (process.env.NODE_ENV === 'production') {
-apiOptions.server = 'https://tralee-tutors.herokuapp.com/';
+apiOptions.server = 'https://tralee-tutors.herokuapp.com';
 }
 
 
