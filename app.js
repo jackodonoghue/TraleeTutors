6440da255a04fd3a97a83d58d6c60f3ca8243469
Jack O'Donoghue <jacklodonoghue@gmail.com>
2019-12-03 20:23:21 +0000

    Signed-off-by: Jack O'Donoghue <jacklodonoghue@gmail.com>

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'images', 'logo.PNG')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
